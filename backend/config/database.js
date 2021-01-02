a6daeb922df317a69fd3247000c81a617d383c73
ghulamabbas123 <safdarhussainshah258@gmail.com>
2021-01-02 14:24:59 +0500

    Fixed database connection string

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI, {
+    mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
